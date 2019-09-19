aebc09e394dac2ee7733fef886d169cf0af450f3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2019-09-19 08:25:26 +0000

    V1 for DIS

---
+++ portal_dis/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Ingeniería Cloud - Pedro Baños
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': "Account docs",
+    'summary': """Añade al área de usuario los elementos requeridos""",
+    'version': '12.0.1.1.0',
+    'category': 'Other',
+    'website': "https://ingenieriacloud.com",
+    'author': "pjb",
+    'license': 'AGPL-3',
+    'installable': True,
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+}
