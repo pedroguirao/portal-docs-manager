633db23d718bff2853b0b97af07ad38e8ee71bef
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2019-12-12 11:21:48 +0000

    report preview

--- portal_green_crm/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ portal_green_crm/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -3,14 +3,17 @@
 # Copyright 2019 Ingeniería Cloud - Pedro Baños
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': "Account docs",
+    'name': "portal green crm",
     'summary': """Añade al área de usuario los elementos requeridos
-    -Di's
+    -DI
+    -NT
+    -CT
+    -Autorizaciones
     """,
-    'version': '11.0.2.1.0',
+    'version': '11.0.5.0.0',
     'category': 'Other',
     'website': "https://ingenieriacloud.com",
-    'author': "pjb",
+    'author': "Ingenieriacloud",
     'license': 'AGPL-3',
     'installable': True,
     'depends': [
@@ -18,5 +21,8 @@
     ],
     'data': [
         'views/templates.xml',
+        'views/templates_ct.xml',
+        'views/templates_nt.xml',
+        'views/templates_au.xml',
     ],
 }
